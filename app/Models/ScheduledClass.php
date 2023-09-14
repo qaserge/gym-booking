e31f3616921588ca2859aefc422c38e82fd6de6c
Sergei <qaserge@gmail.com>
2023-09-14 18:05:14 -0400

    CRUD with views for the ScheduledClass implemented

--- app/Models/ScheduledClass.php
+++ app/Models/ScheduledClass.php
@@ -9,6 +9,12 @@ class ScheduledClass extends Model
 {
     use HasFactory;
 
+    protected $guarded = null; //When $guarded is set to null, it means that all attributes can be bulk filled.
+
+    protected $casts = [ //The $casts property in Laravel is used to define the data types of model attributes when retrieving them from the database and saving them to the database.
+        'date_time' => 'datetime'
+    ];
+
     public function instructor()
     {
         return $this->belongsTo(User::class, 'instructor_id');
