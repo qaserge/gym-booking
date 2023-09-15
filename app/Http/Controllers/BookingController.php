87a78d2eda75c60ba0e6469ec50d9e73c4a7fc6d
Sergei <qaserge@gmail.com>
2023-09-15 14:41:34 -0400

    Booking logic by using Attaching and Detaching relationships implemented

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -12,6 +12,28 @@ class BookingController extends Controller
         $scheduledClasses = ScheduledClass::where('date_time', '>', now())
             ->with('classType', 'instructor') // Eager loading
             ->oldest()->get();
+
         return view('member.book')->with('scheduledClasses', $scheduledClasses);
+    }
+
+    public function store(Request $request)
+    {
+        auth()->user()->bookings()->attach($request->scheduled_class_id);
+
+        return redirect()->route('booking.index');
+    }
+
+    public function index()
+    {
+        $bookings = auth()->user()->bookings()->where('date_time', '>', now())->get();
+
+        return view('member.upcoming')->with('bookings', $bookings);
+    }
+
+    public function destroy(int $id)
+    {
+        auth()->user()->bookings()->detach($id);
+
+        return redirect()->route('booking.index');
     }
 }
