1499b9741047b0014d8e06c029e3fb1b014d5513
Sergei <qaserge@gmail.com>
2023-09-16 15:10:01 -0400

    Added Notification sending for members whose gym-class was cancelled

--- app/Listeners/NotifyClassCanceled.php
+++ app/Listeners/NotifyClassCanceled.php
@@ -4,11 +4,13 @@ namespace App\Listeners;
 
 use App\Events\ClassCanceled;
 use App\Mail\ClassCanceledMail;
+use App\Notifications\ClassCanceledNotification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Notification;
 
 class NotifyClassCanceled
 {
@@ -25,16 +27,18 @@ class NotifyClassCanceled
      */
     public function handle(ClassCanceled $event): void
     {
-        $members = $event->scheduledClass->members();
+        $members = $event->scheduledClass->members()->get();
 
         $className = $event->scheduledClass->classType->name;
         $classDateTime = $event->scheduledClass->date_time;
 
         $details = compact('className', 'classDateTime');
 
-        $members->each(function ($user) use ($details) {
-            $details['userName'] = $user->name;
-            Mail::to($user)->send(new ClassCanceledMail($details));
-        });
+        // $members->each(function ($user) use ($details) {
+        //     $details['userName'] = $user->name;
+        //     Mail::to($user)->send(new ClassCanceledMail($details));
+        // });
+
+        Notification::send($members, new ClassCanceledNotification($details));
     }
 }
